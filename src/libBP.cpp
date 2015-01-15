8f95c594e7438963179cd675406dd8ef3de15a04
Kathryn Rodgers <rodgersk@uci.edu>
2015-01-14 22:47:51 -0800

    trying to get callable library compiling

--- src/libBP.cpp
+++ src/libBP.cpp
@@ -1,6 +1,9 @@
 #include "bp_interface.h"
 #include "jni.h"
 #include "stdio.h"
+#include "BPinterface.h"
+
+
 
 /*
  * Class:     bp_interface
@@ -8,8 +11,28 @@
  * Signature: (Ljava/lang/String;)Z
  */
 JNIEXPORT jboolean JNICALL Java_bp_1interface_doBP (JNIEnv * env, jobject job, jstring fileName){
-  printf("doBP\n");
+printf("doBP\n");
+const char *str= env->GetStringUTFChars(fileName, NULL);
+int len = env->GetStringLength(fileName);
 
+char file[len];
+for(int i=0; i < len; i++){
+file[i] = str[i];
+}
+
+  printf("The string is %s", file);
+
+lgbp::BpInterface* alg = new lgbp::BpInterface();
+ 
+char task[] = {'P','R'};
+  alg->initialize(60, task, file, NULL, NULL, true);
+  alg->runInference();
+ 
+
+  //need to release this string when done with it in
+  //order to avoid memory leak
+  env->ReleaseStringUTFChars(fileName, str);
+ 
   return true;
 }
 
@@ -20,6 +43,11 @@ JNIEXPORT jboolean JNICALL Java_bp_1interface_doBP (JNIEnv * env, jobject job, j
  */
 JNIEXPORT jstring JNICALL Java_bp_1interface_getSolution (JNIEnv * env, jobject jobj){
   printf("getSolution\n");
-  return env->NewStringUTF(":D");
+  double res;
+ //  alg->getSolution(res);
+// FILE* output = fopen("bp_results.txt", "w");
+// fprintf(output, "%g\n", res);
+// fclose(output);
+  return env->NewStringUTF(":O");
   
 }
